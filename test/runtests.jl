e3068f3d8c8c36c343fb8fcd950d20c82d8f68c0
MLackner <lacknersmichael@gmail.com>
2019-07-08 10:41:09 +0200

    working version

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,38 +1,84 @@
-using PyPlot
 using JLD2, FileIO
 using PumpProbeFitting
 using PumpProbeModels
+using Makie, AbstractPlotting
 
-data = load("/Users/lackner/Documents/DataSFG/Notebooks/CaAra/export/pp_d+fr_dif.jld2")
+function plot(m::PumpProbeModel)
+    x = m.wavenumbers
+    y = m.delaytimes
+    z = zeros(length(y), length(x))
+    X, Y, Z = PumpProbeModels.convert1D(x, y, z)
 
-x = data["wn3"]
-y = data["dl"]
-z = data["mdiff3[roi...]"]
+    evaluate!(Z, X, Y, m)
+    z .= reshape(Z, (length(y), length(x))) * -1
 
-m = PumpProbeModel(x, y, 2, pumpmode=:δ)
-m.parameters.A = [4.9, 2.1]
+    scene = wireframe(y, x, z, color=(:red, 1.0))
+    axis = scene[Axis]
+    axis[:names, :axisnames] = ("Pump Delay (ps)", "Wavenumber (1/cm)", "")
+    axis[:names, :rotation] = (1π,  π/2, π/2)
+    axis[:names, :align] = ((:center, :center), (:center, :center), (:center, :center))
+    axis[:names, :gap] = (10, 10)
+    # scale!(scene, 1, 1, 500)
+    # update_cam!(scene, FRect3D(Vec3f0(0), Vec3f0(1)))
+
+    return scene
+end
+
+function plot(m::PumpProbeModel, data)
+    scene = plot(m)
+    z = deepcopy(data) .* -1
+    scatter!(scene, m.delaytimes, m.wavenumbers, z, markersize=2, color=(:black, 0.5))
+
+    # scale!(scene, 1, 1, 500)
+    # update_cam!(scene, FRect3D(Vec3f0(0), Vec3f0(1)))
+
+    scene
+end
+
+# data = load("/Users/lackner/Documents/DataSFG/Notebooks/CaAra/export/pp_d+fr_dif.jld2")
+
+# x = data["wn3"]
+# y = data["dl"]
+# z = data["mdiff3[roi...]"]
+
+x = range(2800.0, 3000.0, length=51)
+y = range(-20, 300, length=61)
+z = zeros(length(y), length(x))
+
+X, Y, Z = PumpProbeModels.convert1D(x, y, z)
+
+m = PumpProbeModel(x, y, 2, pumpmode=:triangle)
+m.pumpfunction = y -> exp(-(y)^2/200)
+m.parameters.A = [4.9, 5.1]
 m.parameters.Γ = [7.0, 8.0]
 m.parameters.ω = [2860.0, 2935.0]
 m.parameters.τ[1] = [90.0, 200.0]
 m.parameters.σ[1] = [0.0, 0.0]
 m.parameters.γ[1] = [0.0, 0.0]
+m.pumptimes = -20.1:19.9
 
-lower, upper = PumpProbeFitting.generate_bounds(m)
+m_data = deepcopy(m)
+# scene = plot(m_data)
 
-r,g = PumpProbeFitting.fit(z, m, lower, upper)
+evaluate!(Z, X, Y, m_data)
+z .= reshape(Z, (length(y), length(x)))
+z .+= (rand(size(z)...) .- 0.5) ./ 50
 
-F = Array{Float64}(undef, length(z[:]))
-xx = eltype(m.wavenumbers)[]
-tt = eltype(m.delaytimes)[]
-for _x in m.wavenumbers, _t in m.delaytimes
-    push!(xx, _x)
-    push!(tt, _t)
-end
-evaluate!(F, xx, tt, m)
-FR = reshape(F, size(z))
 
-figure(figsize=(9,4))
-subplot(121)
-pcolormesh(z)
-subplot(122)
-pcolormesh(FR)
+m_fitstart = deepcopy(m)
+m_fitstart.parameters.A = [4.7, 5.0]
+m_fitstart.parameters.ω = [2863.0, 2931.0]
+# plot(m_fitstart)
+
+m_fit = deepcopy(m_fitstart)
+
+lower, upper = PumpProbeFitting.generate_bounds(m_fit)
+
+r,g = PumpProbeFitting.fit(z, m_fit, lower, upper)
+
+
+scene = plot(m_fit, z)
+
+scale!(scene, 1, 1, 400)
+update_cam!(scene, FRect3D(Vec3f0(0), Vec3f0(1)))
+scene
